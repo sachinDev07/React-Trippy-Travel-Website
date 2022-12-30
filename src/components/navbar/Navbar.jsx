7ca5660260a9b4b256cecb28d4bea1f926979628
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-30 13:25:01 +0500

    fixed some issues

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 import './navbar.scss'
 import { data } from './ListData'
-
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
-  const [menuOpen, setMenuOpen] = useState(true)
+  const [menuOpen, setMenuOpen] = useState(false)
 
   const handleClick = () => {
     setMenuOpen(!menuOpen)
@@ -16,9 +16,10 @@ const Navbar = () => {
         <h1 className='navbar__logo'>Trippy</h1>
         <ul className={menuOpen ? "navbar__menu active" : "navbar__menu"} >
             {data.map((d)=>(
-                <li className={d.cName}><i className={d.icon}></i> {d.title}</li>
+                <li>
+                <Link to={d.url} key={d.title} className={d.cName}><i className={d.icon}></i> {d.title}</Link>
+                </li>
             ))} 
-
             <button>Sign Up</button> 
         </ul>
         <div menuOpen={menuOpen} onClick={()=>handleClick()} className="hamburger">
