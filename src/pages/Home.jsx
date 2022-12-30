85d2e5f2f02e6806827caee333ff44d1c9b61f49
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-30 23:37:54 +0500

    fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import Hero from '../components/homePage/hero/Hero'
  
 
 const Home = () => {
   return (
-    <div>
-        
-    </div>
+    <>
+       <Hero
+        cName="hero"
+        heroImg="https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
+        title="Your Journey Your Story"
+        text="Choose Your Favourite Destinations"
+        buttonText="Travel Plan"
+        url="/"
+        btnClass="show"
+       />
+    </>
   )
 }
 
